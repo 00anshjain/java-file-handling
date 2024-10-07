2c103d20f7b54e90e55267a4ed366f8251f95980
AnshJain <anshj@tekion.com>
2024-10-08 00:29:57 +0530

    second commit

---
+++ FileHandling2/src/test/java/filehandling/filehandling2/FileHandling2ApplicationTests.java
@@ -0,0 +1,13 @@
+package filehandling.filehandling2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileHandling2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
